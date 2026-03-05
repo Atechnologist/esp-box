be629edc19abc5b4596afc12e63cfa631ec4ab0c
Atechnologist <117396502+Atechnologist@users.noreply.github.com>
2026-03-05 15:46:55 -0500

    Update wifi_connect.h

--- examples/factory_demo/main/wifi_connect.h
+++ examples/factory_demo/main/wifi_connect.h
@@ -1,37 +1,3 @@
-#include "wifi_connect.h"
-#include "esp_wifi.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "esp_netif.h"
-#include "esp_event.h"
+#pragma once
 
-static const char *TAG = "wifi";
-
-#define WIFI_SSID "YOUR_WIFI"
-#define WIFI_PASS "YOUR_PASSWORD"
-
-void wifi_connect(void)
-{
-    ESP_LOGI(TAG, "Initializing WiFi");
-
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PASS
-        }
-    };
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-
-    ESP_LOGI(TAG, "Connecting to WiFi...");
-
-    ESP_ERROR_CHECK(esp_wifi_start());
-}
+void wifi_connect(void);
