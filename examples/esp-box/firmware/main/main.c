fe11dd4121b2a0a4db66359be57c013092f51a2b
Atechnologist <117396502+Atechnologist@users.noreply.github.com>
2026-03-25 12:17:13 -0400

    Update main.c

--- examples/esp-box/firmware/main/main.c
+++ examples/esp-box/firmware/main/main.c
@@ -13,8 +13,8 @@
 static const char *TAG = "ESP_BOX_PROV";
 
 /* ================= EVENT HANDLER ================= */
-static void prov_event_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data)
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_PROV_EVENT) {
         switch (event_id) {
@@ -25,36 +25,31 @@ static void prov_event_handler(void* arg, esp_event_base_t event_base,
 
         case WIFI_PROV_CRED_RECV: {
             wifi_sta_config_t *wifi_sta_cfg = (wifi_sta_config_t *)event_data;
-            ESP_LOGI(TAG, "Received WiFi SSID: %s",
-                     (const char *) wifi_sta_cfg->ssid);
+            ESP_LOGI(TAG, "Received SSID: %s", (char *)wifi_sta_cfg->ssid);
             break;
         }
 
-        case WIFI_PROV_CRED_FAIL:
-            ESP_LOGE(TAG, "Provisioning failed. Try again");
+        case WIFI_PROV_CRED_SUCCESS:
+            ESP_LOGI(TAG, "Provisioning successful");
             break;
 
-        case WIFI_PROV_CRED_SUCCESS:
-            ESP_LOGI(TAG, "Provisioning successful!");
+        case WIFI_PROV_CRED_FAIL:
+            ESP_LOGE(TAG, "Provisioning failed");
             break;
 
         case WIFI_PROV_END:
             ESP_LOGI(TAG, "Provisioning ended");
             wifi_prov_mgr_deinit();
             break;
-
-        default:
-            break;
         }
     }
 
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        ESP_LOGI(TAG, "Connecting to WiFi...");
         esp_wifi_connect();
     }
 
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        ESP_LOGW(TAG, "Disconnected. Retrying...");
+        ESP_LOGW(TAG, "Retrying WiFi...");
         esp_wifi_connect();
     }
 
@@ -67,31 +62,25 @@ static void prov_event_handler(void* arg, esp_event_base_t event_base,
 /* ================= MAIN ================= */
 void app_main(void)
 {
-    /* Initialize NVS */
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ESP_ERROR_CHECK(nvs_flash_init());
     }
 
-    /* Init network stack */
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    /* Create default interfaces */
     esp_netif_create_default_wifi_sta();
     esp_netif_create_default_wifi_ap();
 
-    /* Init WiFi */
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    /* Register events */
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_PROV_EVENT, ESP_EVENT_ANY_ID, &prov_event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &prov_event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &prov_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_PROV_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
 
-    /* Provisioning config */
     wifi_prov_mgr_config_t config = {
         .scheme = wifi_prov_scheme_softap,
         .scheme_event_handler = WIFI_PROV_EVENT_HANDLER_NONE
@@ -104,30 +93,25 @@ void app_main(void)
 
     if (!provisioned) {
 
-        ESP_LOGI(TAG, "Starting provisioning...");
+        ESP_LOGI(TAG, "Starting provisioning");
 
         ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(
             WIFI_PROV_SECURITY_1,
-            "abcd1234",          // Proof of possession (POP)
-            "ESP-BOX-PROV",      // AP name
-            "12345678"           // AP password
+            "abcd1234",
+            "ESP-BOX-PROV",
+            "12345678"
         ));
 
-        ESP_LOGI(TAG, "=================================");
         ESP_LOGI(TAG, "Connect to WiFi: ESP-BOX-PROV");
-        ESP_LOGI(TAG, "Password: 12345678");
-        ESP_LOGI(TAG, "Open browser: http://192.168.4.1");
-        ESP_LOGI(TAG, "=================================");
+        ESP_LOGI(TAG, "Open: http://192.168.4.1");
 
     } else {
 
-        ESP_LOGI(TAG, "Already provisioned. Connecting...");
+        ESP_LOGI(TAG, "Already provisioned, connecting...");
 
         wifi_prov_mgr_deinit();
 
         ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
         ESP_ERROR_CHECK(esp_wifi_start());
-
-        ESP_LOGI(TAG, "WiFi started in STA mode");
     }
 }
